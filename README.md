2a9ef4c27e0a0faf5401af8eb0cfd6586c17263e
youngchief btw ツ <33383463+youngchief-btw@users.noreply.github.com>
2020-04-17 17:34:09 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# ProxyChief
+
+---
+
+**Made by [youngchief btw ツ](https://github.com/youngchief-btw), with ❤️**
+
+---
+
+**This proxy is made to be the very best, and is super lightweight.**
+
+---
+
+**_Access using any of these [URLs](/ListOfInstances.txt)_**
+
+---
+
+Format for entries: `protocol://domain:port/`
+
+`protocol` is `http` or `https`.
+
+`domain` is the domain (subdomains count as their own different domain) or IP.
+
+`port` is the port it is running (make a new entry for each port).
+
+---
+
+Run your own instance:
+
+Run `wget -O proxychief.zip https://github.com/youngchief-btw/ProxyChief/archive/master.zip` (I recommend doing `git clone https://github.com/youngchief-btw/ProxyChief.git` instead to pull in the latest changes)
---
+++ src/ProxyChief.js
@@ -0,0 +1,42 @@
+/*
+ * ProxyChief
+ * © 2020 youngchief btw ツ, All rights reserved.
+ */
+const https = require("https");
+const http = require("http");
+const fs = require("fs");
+const Unblocker = require("unblocker");
+const unblocker = Unblocker({});
+const express = require("express");
+const app = express();
+const httpProxy = require("http-proxy");
+var proxy = httpProxy.createProxyServer({});
+
+function miniCPU() {
+  // This function lowers memory, disk?, and CPU on request
+  process.exit();
+  process.kill();
+  process.abort();
+}
+
+// http.createServer(app).listen(80);
+// https.createServer(null, app).listen(443);
+
+// this must be one of the first app.use() calls and must not be on a subdirectory to work properly
+app.use(new Unblocker({ prefix: "/proxy/" }));
+app.get("/proxy/", function(req, res) {
+  miniCPU();
+});
+
+app.get("/", function(req, res) {
+  res.send(
+    '<style>*{font-family: sans-serif;}</style><h1>ProxyChief</h1>Proxy Format: <b>/proxy/SITE-TO-VISIT</b><hr><p><strong>Made by <a href="https://github.com/youngchief-btw">youngchief btw ツ</a>, with ❤️</strong></p><hr><p><strong>This proxy is made to be the very best, and is super lightweight.</strong></p><hr><p><strong><em>Access using any of these <a href="/proxy/https://github.com/youngchief-btw/ProxyChief/blob/master/ListOfInstances.txt">URLs</a></em></strong></p>'
+  );
+});
+
+app.listen(8080);
+
+// app.listen = function() {
+//   var server = http.createServer(this);
+//   return server.listen.apply(server, arguments);
+// };
